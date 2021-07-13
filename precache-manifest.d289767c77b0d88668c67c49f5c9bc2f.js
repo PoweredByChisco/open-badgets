db5d38a783449405f2e9a3c5a84407f60f9dd4bc
Francisco <francisco.marquez.solt@gmail.com>
2021-07-13 17:41:44 -0500

    Updates

---
+++ precache-manifest.d289767c77b0d88668c67c49f5c9bc2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0186b3296aefc4bb40df",
+    "url": "/open-badgets/static/css/main.747a1973.chunk.css"
+  },
+  {
+    "revision": "0186b3296aefc4bb40df",
+    "url": "/open-badgets/static/js/main.0186b329.chunk.js"
+  },
+  {
+    "revision": "2e7998f60148c415cac5",
+    "url": "/open-badgets/static/js/runtime~main.2e7998f6.js"
+  },
+  {
+    "revision": "490b31c0087cc53f2b0a",
+    "url": "/open-badgets/static/css/2.2ce097e2.chunk.css"
+  },
+  {
+    "revision": "490b31c0087cc53f2b0a",
+    "url": "/open-badgets/static/js/2.490b31c0.chunk.js"
+  },
+  {
+    "revision": "b6282b8433ab9d184962f70ae7d51585",
+    "url": "/open-badgets/static/media/conf-logo.b6282b84.png"
+  },
+  {
+    "revision": "82d9f293adbbafd277c5f675f3839768",
+    "url": "/open-badgets/index.html"
+  }
+];
